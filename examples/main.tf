a3141b0c94cd4f8eb7a25cbce038389a5605615e
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-05-15 17:18:39 +0100

    Pass cluster_domain_name when calling the module

--- examples/main.tf
+++ examples/main.tf
@@ -6,5 +6,6 @@ module "eso" {
   source = "../"
     dependence_prometheus           = "ignore"
     #dependence_prometheus           = module.monitoring.prometheus_operator_crds_status
+    cluster_domain_name             = "eso.cloud-platform.service.justice.gov.uk"
     secrets_prefix = "test"
 }
