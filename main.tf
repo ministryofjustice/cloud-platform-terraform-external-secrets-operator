f8bd098b36fdcbe27f0f686940300973d68e5da0
sj-williams <steve.williams@digital.justice.gov.uk>
2023-10-06 12:22:57 +0100

    add psa audit restricted label to eso namespace

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ resource "kubernetes_namespace" "external_secrets_operator" {
       "cloud-platform.justice.gov.uk/environment-name" = "production"
       "cloud-platform.justice.gov.uk/is-production"    = "true"
       "certmanager.k8s.io/disable-validation"          = "true"
+      "pod-security.kubernetes.io/audit"               = "restricted"
     }
 
     annotations = {
